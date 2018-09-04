ccde789f6f6e19842fed16322903b16b5951c8c3
QuintenSmith <quintensmith15@gmail.com>
2018-09-04 09:50:10 -0700

    first commit

---
+++ PokemonSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonSearch
+//
+//  Created by Quinten Smith on 9/4/18.
+//  Copyright © 2018 Quinten Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
